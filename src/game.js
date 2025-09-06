1e46dd6c2c035f694078d51c8bcb252304132a12
Uladzislau Khamkou <kham@tuta.io>
2025-09-06 12:08:00 +0300

    Add batched rendering of quads

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 const QUADS_VS = await(await fetch("./quads.vs.glsl", { cache: "no-store" })).text();
 const QUADS_FS = await(await fetch("./quads.fs.glsl", { cache: "no-store" })).text();
+const MAX_BUFFER_SIZE = 8;
 
 let canvas = document.createElement("canvas");
 canvas.style = "position: absolute; width: 100%; height: 100%; inset: 0px;";
@@ -11,10 +12,10 @@ let glIndicesBuffer = gl.createBuffer(); {
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
 };
 
-let vertices = gl.createBuffer(); {
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertices);
-	const positions = new Float32Array([-1, -1, -1, 1, 1, 1, 1, -1]);
-	gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+let vertices = new Float32Array([-1, -1, -1, 1, 1, 1, 1, -1]);
+let verticesBuffer = gl.createBuffer(); {
+	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STREAM_DRAW);
 }
 
 function buildShaderProgram(vs, fs) {
@@ -59,16 +60,41 @@ function onFrame(t) {
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	gl.useProgram(shader);
-	gl.bindBuffer(gl.ARRAY_BUFFER, (vertices));
-	gl.vertexAttribPointer(0, 2, gl.FLOAT, 0, 0, 0);
-	gl.enableVertexAttribArray(0);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, glIndicesBuffer);
-	gl.uniform2f(gl.getUniformLocation(shader, "scale"), 640 / canvas.clientWidth, 640 / canvas.clientHeight);
-	gl.drawElements(gl.TRIANGLE_STRIP, 6, gl.UNSIGNED_SHORT, 0);
+	t /= 1000;
+	drawQuad(Math.sin(t), Math.cos(t), 1, 1, 1);
+	drawQuad(-Math.sin(t), -Math.cos(t), 2, 1, 1);
+	drawBatch();
 
 	window.requestAnimationFrame(onFrame);
 	last_t = t;
+}
+
+let bufferSize = 0;
+function drawQuad(x, y, z, w, h) {
+	if (bufferSize == MAX_BUFFER_SIZE) drawBatch();
+	let dx = w * z / 2;
+	let dy = h * z / 2;
+	vertices[bufferSize++] = x - dx;
+	vertices[bufferSize++] = y - dy;
+	vertices[bufferSize++] = x - dx;
+	vertices[bufferSize++] = y + dy;
+	vertices[bufferSize++] = x + dx;
+	vertices[bufferSize++] = y + dy;
+	vertices[bufferSize++] = x + dx;
+	vertices[bufferSize++] = y - dy;
+}
+
+function drawBatch() {
+	if (bufferSize == 0) return;
+	gl.useProgram(shader);
+	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STREAM_DRAW);
+	gl.vertexAttribPointer(0, 2, gl.FLOAT, 0, 0, 0);
+	gl.enableVertexAttribArray(0);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, glIndicesBuffer);
+	gl.uniform2f(gl.getUniformLocation(shader, "scale"), 320 / canvas.clientWidth, 320 / canvas.clientHeight);
+	gl.drawElements(gl.TRIANGLE_STRIP, bufferSize * 3 >> 2, gl.UNSIGNED_SHORT, 0);
+	bufferSize = 0;
 }
 
 window.requestAnimationFrame(onFrame);
